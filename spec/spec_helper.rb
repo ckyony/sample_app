99cda0f872efb1461b7abdb2487ea80370266862
ckyony <ckyony@gmail.com>
2010-08-04 11:42:39 +0200

    After spork

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,27 +1,28 @@
-# This file is copied to ~/spec when you run 'ruby script/generate rspec'
-# from the project root directory.
-ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../../config/environment", __FILE__)
-require 'rspec/rails'
+require 'rubygems'
+require 'spork'
 
-# Requires supporting files with custom matchers and macros, etc,
-# in ./support/ and its subdirectories.
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+Spork.prefork do
+  # Loading more in this block will cause your tests to run faster. However, 
+  # if you change any configuration or code from libraries loaded here, you'll
+  # need to restart spork for it take effect.
+    ENV["RAILS_ENV"] ||= 'test'
+    unless defined?(Rails)
+	require File.dirname(__FILE__) +"/../config/environment"
+    end
+    require 'rspec/rails'
 
-RSpec.configure do |config|
-  # == Mock Framework
-  #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
-  config.mock_with :rspec
+    # Requires supporting files with custom matchers and macros, etc,
+    # in ./support/ and its subdirectories
+    Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+    Rspec.configure do |config|
+	config.mock_with :rspec
 
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, comment the following line or assign false
-  # instead of true.
-  config.use_transactional_fixtures = true
+	config.fixture_path = "#{::Rails.root}/spec/fixtures"
+	# 
 end
+
+Spork.each_run do
+  # This code will be run each time you run your specs.
+  
+end
+
