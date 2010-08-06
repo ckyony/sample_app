4b16d08ea7f805c84a415de9f18b1fdd129b3779
ckyony <ckyony@gmail.com>
2010-08-06 16:42:16 +0200

    before section 5.3

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 SampleApp::Application.routes.draw do |map|
-	match 'contact', :to => 'pages#contact'
-	match 'about', :to => 'pages#about'
-	match 'help', :to => 'pages#help'
+	match '/contact', :to => 'pages#contact'
+	match '/about', :to => 'pages#about'
+	match '/help', :to => 'pages#help'
 
   
   # The priority is based upon order of creation:
@@ -53,7 +53,7 @@ SampleApp::Application.routes.draw do |map|
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => "pages#home"
+   root :to => 'pages#home'
 
   # See how all your routes lay out with "rake routes"
 
