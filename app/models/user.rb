dd4f8b86e963a20cafa34011f7d9614214fd1ebf
ckyony <ckyony@gmail.com>
2010-08-09 09:57:36 +0200

    before 6.2.2

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+# Schema version: 20100809062329
+#
+# Table name: users
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
+class User < ActiveRecord::Base
+    attr_accessible :name, :email
+
+    validates :name, :presence => true
+    validates :email, :presence => true
+end
